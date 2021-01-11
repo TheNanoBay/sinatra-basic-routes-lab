f85cde16734a2ed7738b8891c1fd84673a3faf46
TheNanoBay <noorani.mehdi@outlook.com>
2021-01-11 13:59:21 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+      end
+
+    get ('/name') {"My name is Mehdi"}
+
+    get ('/hometown') {"My hometown is Minnesota"}
+
+    get ('/favorite-song') {"My favorite song is work"}
+
+
 end
